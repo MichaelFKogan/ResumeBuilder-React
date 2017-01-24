ed98453d4c4e36a38ef0d3d822595994534cb331
MichaelFKogan <mfkogan@gmail.com>
2017-01-24 14:34:32 -0500

    https indeed not working. Need http

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -11,22 +11,22 @@ runQuery: function(location, keyword) {
 
 if(keyword && location){
     console.log("1");
-    var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q="+ keyword +"&l="+ location +"&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
+    var queryURL = "http://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q="+ keyword +"&l="+ location +"&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
 }
 
 else if(keyword){
     console.log("2");
-    var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q="+ keyword +"&l=NewYork,NY&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
+    var queryURL = "http://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q="+ keyword +"&l=NewYork,NY&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
 }
 
 else if (location){
     console.log("3");
-    var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q=&l="+ location +"&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
+    var queryURL = "http://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q=&l="+ location +"&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
 }
 
 else {
     console.log("4");
-    var queryURL = "https://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q=&l=NewYork,NY&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
+    var queryURL = "http://api.indeed.com/ads/apisearch?publisher=4548195452860771&v=2&format=json&q=&l=NewYork,NY&sort=date&radius=25&start=0&limit=25&latlong=1&co=us&userip=1.2.3.4&useragent=GoogleChrome&v=2"
 }
 
         return axios.get(queryURL).then(function(response) {
